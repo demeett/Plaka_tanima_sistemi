37d91b3a0f18d14e901f4508fd019625820d1330
demeett <demirdemet96@gmail.com>
2021-03-12 18:49:44 +0300

    Plaka Tanıma Sistemi
    Araçların sisteme kaydını , girişini, silme işlemlerini yapan bir projedir. Buna göre veritabanında kayıtlı olan araçlarından geçişine izin verecek ya da vermeyecektir.

---
+++ OpenALPR-PlakaTanima/OpenALPR-PlakaTanima/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace PlakaTanima
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picOriginResim = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPlaka = new System.Windows.Forms.TextBox();
+            this.picPlakaResmi = new System.Windows.Forms.PictureBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.KontrolClick = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginResim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlakaResmi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picOriginResim
+            // 
+            this.picOriginResim.Location = new System.Drawing.Point(19, 41);
+            this.picOriginResim.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.picOriginResim.Name = "picOriginResim";
+            this.picOriginResim.Size = new System.Drawing.Size(343, 345);
+            this.picOriginResim.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picOriginResim.TabIndex = 0;
+            this.picOriginResim.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(442, 41);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(73, 21);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Avrupa";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(581, 41);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(84, 21);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Amerika";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.button1.Location = new System.Drawing.Point(442, 77);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Resim Seç";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPlaka
+            // 
+            this.txtPlaka.Location = new System.Drawing.Point(442, 160);
+            this.txtPlaka.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtPlaka.Name = "txtPlaka";
+            this.txtPlaka.Size = new System.Drawing.Size(236, 22);
+            this.txtPlaka.TabIndex = 4;
+            this.txtPlaka.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // picPlakaResmi
+            // 
+            this.picPlakaResmi.Location = new System.Drawing.Point(445, 246);
+            this.picPlakaResmi.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.picPlakaResmi.Name = "picPlakaResmi";
+            this.picPlakaResmi.Size = new System.Drawing.Size(233, 141);
+            this.picPlakaResmi.TabIndex = 5;
+            this.picPlakaResmi.TabStop = false;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Resim";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(442, 129);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Bulunan Plaka";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(442, 217);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Plaka Resmi";
+            // 
+            // KontrolClick
+            // 
+            this.KontrolClick.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.KontrolClick.Location = new System.Drawing.Point(753, 153);
+            this.KontrolClick.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.KontrolClick.Name = "KontrolClick";
+            this.KontrolClick.Size = new System.Drawing.Size(152, 37);
+            this.KontrolClick.TabIndex = 9;
+            this.KontrolClick.Text = "Kontrol Et";
+            this.KontrolClick.UseVisualStyleBackColor = false;
+            this.KontrolClick.Click += new System.EventHandler(this.KontrolClick_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.button3.Location = new System.Drawing.Point(753, 77);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 35);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Plaka Kaydet";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(919, 548);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.KontrolClick);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picPlakaResmi);
+            this.Controls.Add(this.txtPlaka);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.picOriginResim);
+            this.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.Text = "Plaka Tanıma";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginResim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlakaResmi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picOriginResim;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPlaka;
+        private System.Windows.Forms.PictureBox picPlakaResmi;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button KontrolClick;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ OpenALPR-PlakaTanima/OpenALPR-PlakaTanima/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using openalprnet;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+
+
+namespace PlakaTanima
+{
+    public partial class Form1 : Form
+
+    {
+        SqlConnection baglanti = new SqlConnection("Data Source=.; Initial Catalog=PlakaTanim; Integrated Security=true");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                var codeBase = Assembly.GetExecutingAssembly().CodeBase; 
+                                                                         
+                                                                         
+                var uri = new UriBuilder(codeBase);
+                var path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+        public Rectangle boundingRectangle(List<Point> points)//resmi küçültüp büyütüyo //kenar bulmayı yapıyor.
+        {
+            var minX = points.Min(p => p.X); // Sol taraftaki giriş değişkenlerini sağ taraftaki lambda gövdesinden ayırmak için kullanılır.en küçüğü bulur.
+            var minY = points.Min(p => p.Y);
+            var maxX = points.Max(p => p.X);
+            var maxY = points.Max(p => p.Y);
+
+            return new Rectangle(new Point(minX, minY), new Size(maxX - minX, maxY - minY));
+        }
+
+        private static Image cropImage(Image img, Rectangle cropArea) // görüntüyü kırpıyorum. //croparea kırpma alanı
+        {
+            var bmpImage = new Bitmap(img);
+            return bmpImage.Clone(cropArea, bmpImage.PixelFormat); //resmi pixellerine ayırdı.çoğalttı.
+        }
+
+        public static Bitmap combineImages(List<Image> images) //tüm görüntüleri belleğe okuyor.
+        {
+            Bitmap finalImage = null;
+            // plkayaı okurken parçalıyo ve parçaları birbirine ekliyo teker teker.bileşenlerine ayırıyor.""
+            try
+            {
+                var width = 0;
+                var height = 0;
+
+                foreach (var bmp in images)
+                {
+                    width += bmp.Width;
+                    height = bmp.Height > height ? bmp.Height : height;
+                }
+                //birleştirilen görüntüyü  tutmak için bitmap oluştur.
+                finalImage = new Bitmap(width, height);
+                using (var g = Graphics.FromImage(finalImage))//görüntüden bir grafik nesnesi elde edelim
+
+                {
+                    g.Clear(Color.Black);
+                    var offset = 0; //her görüntüden geçip son görüntüye çiz
+
+                    foreach (Bitmap image in images)
+                    {
+                        g.DrawImage(image,
+                                    new Rectangle(offset, 0, image.Width, image.Height));
+                        offset += image.Width;
+                    }
+                }
+
+                return finalImage;
+            }
+            catch (Exception ex)
+            {
+                if (finalImage != null)
+                    finalImage.Dispose();
+
+                throw ex;
+            }
+            finally //belleği temizle
+            {
+                foreach (var image in images)
+                {
+                    image.Dispose();
+                }
+            }
+        }
+
+        private void processImageFile(string fileName)//resim seç butonuna tıklanınca seçilenn resmin yolu string filename olarak attım.
+        {
+            txtPlaka.Text = "";
+            resetControls();// bütün resimler boşta.
+            var region = radioButton2.Checked ? "us" : "eu";
+            String config_file = Path.Combine(AssemblyDirectory, "openalpr.conf"); //yol oluşturma asıl burada başlıyor.assembly burada uri örneği oluşturuyor.
+            String runtime_data_dir = Path.Combine(AssemblyDirectory, "runtime_data");
+            using (var alpr = new AlprNet(region, config_file, runtime_data_dir))
+            {
+                if (!alpr.IsLoaded())
+                {
+                    txtPlaka.Text = "Error initializing OpenALPR";
+                    return;
+                }
+                picOriginResim.ImageLocation = fileName;                                     // seçtiğim yoldaki resmi ana ekrana alıyorum
+                picOriginResim.Load();                                                       //resmi yükleme yapıyor.
+
+                var results = alpr.Recognize(fileName);                                     //tanımayı yapıyor.
+
+                var images = new List<Image>(results.Plates.Count()); // plaka sayısını atıyor.
+                var i = 1;
+                foreach (var result in results.Plates)                                                  //Resim düzenlemesi yapıyor.//düzenlenen resmi de diğer picboxa atıcak.
+                {
+                    var rect = boundingRectangle(result.PlatePoints);                                   // resmi çerçeveleyecek.
+                    var img = Image.FromFile(fileName);
+                    var cropped = cropImage(img, rect);
+                    images.Add(cropped);            //Buralar bulduğu plaka sayısına göre döngüye giriyor.
+
+                    txtPlaka.Text = EnBenzeyenPlakayiGetir(result.TopNPlates);
+                }
+
+                if (images.Any())
+                {
+                    picPlakaResmi.Image = combineImages(images);                               // oluşturulan resmi picplakaresmine attım.
+                }
+            }
+        }
+
+        private string EnBenzeyenPlakayiGetir(List<AlprPlateNet> plakalar) //Plakayı bulmaya çalışıyyor en çok neye benziyor karakterler diye.
+        {
+            foreach (var item in plakalar)
+            {
+                return item.Characters.PadRight(12);
+            }
+            return "";
+        }
+
+        private void resetControls()
+        {
+            picOriginResim.Image = null;
+            picPlakaResmi.Image = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resetControls();
+        }
+
+      
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK) //kontrol metodum. REsimler boş mu dolu mu onnu kontrol edecek.
+            {
+                processImageFile(openFileDialog.FileName); //resim seçtirdim .Proccessımagefile methoduna attım.
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=DEMET\\SQLEXPRESS;Initial Catalog=PlakaTanima;Integrated Security=True");
+            if (baglanti.State == ConnectionState.Closed) 
+                baglanti.Open();
+                string kayıt = "insert into dbo.plaka(Plakalar) values(@çekilen_plaka)";
+                SqlCommand cmd = new SqlCommand(kayıt, baglanti);
+                cmd.Parameters.AddWithValue("@çekilen_plaka", txtPlaka.Text);
+                cmd.ExecuteNonQuery();
+                baglanti.Close();
+                MessageBox.Show("Plaka Kayıt İşlemi Gerçekleşti.");
+        }
+
+        private void KontrolClick_Click(object sender, EventArgs e)
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=DEMET\\SQLEXPRESS;Initial Catalog=PlakaTanima;Integrated Security=True");
+
+            if (baglanti.State == ConnectionState.Closed) 
+                 baglanti.Open();
+                 SqlCommand cmd = new SqlCommand("select Plakalar from plaka",baglanti);
+                    cmd.Parameters.AddWithValue("@Plakalar", txtPlaka.Text);
+                    cmd.Connection = baglanti;
+                 SqlDataReader sdr = cmd.ExecuteReader();
+            if (sdr.Read())
+            {
+                txtPlaka.Text = sdr["Plakalar"].ToString();
+                MessageBox.Show("geçebilir");
+            }
+            else
+                MessageBox.Show("geçemez");
+
+
+            
+                 baglanti.Close();
+
+        }
+    }
+}
